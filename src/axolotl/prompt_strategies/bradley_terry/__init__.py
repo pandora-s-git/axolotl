dd98db973ff86cb6ed42ba11de5861a0397b2582
Wing Lian <wing.lian@gmail.com>
2024-08-27 20:22:38 -0400

    fix BT rm loader

--- src/axolotl/prompt_strategies/bradley_terry/__init__.py
+++ src/axolotl/prompt_strategies/bradley_terry/__init__.py
@@ -1,8 +1,34 @@
-"""
-module for bradley terry reward model style dataset transform strategies
-"""
-from functools import partial
+"""Module to load prompt strategies."""
 
-from ..base import load as load_base
+import importlib
+import inspect
+import logging
 
-load = partial(load_base, module_base="axolotl.prompt_strategies.bradeley_terry")
+from axolotl.prompt_strategies.user_defined import UserDefinedDatasetConfig
+
+LOG = logging.getLogger("axolotl.prompt_strategies")
+
+
+def load(strategy, tokenizer, cfg, ds_cfg):
+    try:
+        load_fn = "load"
+        if strategy.split(".")[-1].startswith("load_"):
+            load_fn = strategy.split(".")[-1]
+            strategy = ".".join(strategy.split(".")[:-1])
+        mod = importlib.import_module(
+            f".{strategy}", "axolotl.prompt_strategies.bradley_terry"
+        )
+        func = getattr(mod, load_fn)
+        load_kwargs = {}
+        if strategy == "user_defined":
+            load_kwargs["ds_cfg"] = UserDefinedDatasetConfig(**ds_cfg)
+        else:
+            sig = inspect.signature(func)
+            if "ds_cfg" in sig.parameters:
+                load_kwargs["ds_cfg"] = ds_cfg
+        return func(tokenizer, cfg, **load_kwargs)
+    except ModuleNotFoundError:
+        return None
+    except Exception as exc:  # pylint: disable=broad-exception-caught
+        LOG.error(f"Failed to load prompt strategy `{strategy}`: {str(exc)}")
+        return None
